030b70e1648940d3b1220c961d31e89ae4fd5558
Sam_lll <sam@signature-entertainment.co.uk>
2023-02-21 22:02:40 +0000

    Initial Commit

---
+++ semantic.py
@@ -0,0 +1,36 @@
+import spacy
+nlp = spacy.load('en_core_web_md')
+
+word1 = nlp("cat")
+word2 = nlp("monkey")
+word3 = nlp("banana")
+
+print(word1.similarity(word2))
+print(word3.similarity(word2))
+print(word3.similarity(word1))
+
+tokens = nlp('cat apple monkey banna ')
+
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
+
+sentence_to_compare = "Why is my cat on the car"
+
+sentences = ["Where did my dog go",
+"Hell, there is my car",
+"I\'ve lost my car in my car",
+"I\'d like my boat back",
+"I will name my dog Diana"]
+
+model_sentence = nlp(sentence_to_compare)
+
+for sentence in sentences:
+    similarity = nlp(sentence).similarity(model_sentence)
+    print(sentence + " - ", similarity)
+
+# Interesting that the model considers the animals "cat" and "monkey" more similar than "monkey" and its food source "banana"
+# My example
+my_word1 = nlp("tiger")
+my_word2 = nlp("lion")
+print(my_word1.similarity(my_word2))
